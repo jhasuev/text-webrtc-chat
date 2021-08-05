4e79e39ea86c8bc2391856b9d77b3545f8f84b9f
Jamal Hasuev <metafra@mail.ru>
2021-08-06 01:34:13 +0300

    add Search layout

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,11 @@
 <template>
-  Search.vue
+  <div class="text-center">
+    <h4 class="mb-3">Поиск собеседника</h4>
+    <div class="mb-4">
+      <div class="spinner-border text-secondary" role="status"></div>
+    </div>
+    <button class="btn btn-primary">Остановить поиск</button>
+  </div>
 </template>
 
 <script>
