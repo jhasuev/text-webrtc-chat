8eab392e8a607485940923d26e09f0244b08e69e
Jamal Hasuev <metafra@mail.ru>
2021-08-06 01:22:48 +0300

    wrapper for all routes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <router-view />
+  <div class="container">
+    <div class="row">
+      <div class="col-12 py-2">
+
+        <div class="card">
+          <div class="card-body">
+            <router-view />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +19,10 @@ export default {
   name: 'App',
 }
 </script>
+
+<style scoped lang="scss">
+.card {
+  max-width: 444px;
+  margin: auto;
+}
+</style>
