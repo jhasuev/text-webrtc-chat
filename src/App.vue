cf1842a5230c02c258ac93d1f8b09997c106ecc6
Jamal Hasuev <metafra@mail.ru>
2021-08-06 14:26:07 +0300

    add Chat layout + change wrapper styles for /chat route

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 py-2">
-
-        <div class="card">
-          <div class="card-body">
+  <div class="container  h-100">
+    <div class="row h-100">
+      <div class="col-12 py-2 h-100">
+        <div class="card" :style="fitHeight">
+          <div class="card-body" :style="fitHeight">
             <router-view />
           </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -17,6 +15,11 @@
 <script>
 export default {
   name: 'App',
+  computed: {
+    fitHeight(){
+      return this.$route.name === 'chat' ? { height: "100%" } : ''
+    },
+  },
 }
 </script>
 
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,5 +1,41 @@
 <template>
-  Chat.vue
+  <div class="chat">
+    <div class="d-flex justify-content-between mb-2">
+      <span>Диалог с собеседником</span>
+      <button type="button" class="btn-close ms-3" aria-label="Close" title="Завершить диалог"></button>
+    </div>
+
+    <div class="chat__messages">
+      <div class="chat__messages-item  chat__messages-item--me" v-for="n in 100" :key="n">
+        <div class="chat__messages-text">
+          Привет!!! Как дела?
+        </div>
+        <time class="chat__messages-time">02:27</time>
+      </div>
+
+      <div class="chat__messages-item  chat__messages-item--companion">
+        <div class="chat__messages-text">
+          Нормально! Сам как ?
+        </div>
+        <time class="chat__messages-time">02:27</time>
+      </div>
+
+      <div class="chat__messages-item  chat__messages-item--companion">
+        <div class="chat__messages-text">
+          eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        </div>
+        <time class="chat__messages-time">02:27</time>
+      </div>
+    </div>
+    
+
+    <div class="chat__actions">
+      <div class="chat__actions-message">
+        <textarea class="form-control" placeholder="Ваше сообщение..."></textarea>
+      </div>
+      <button class="btn btn-primary ms-2">Отправить</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +43,63 @@ export default {
   name: "Chat",
 }
 </script>
+
+<style lang="scss" scoped>
+
+.chat {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+
+  &__messages {
+    flex-grow: 1;
+
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    padding: 10px;
+    background-color: #f6f6f6;
+    border: 1px solid #ccc;
+
+    overflow-y: auto;
+
+    &-item {
+      margin-bottom: 10px;
+      max-width: 80%;
+
+      border-radius: 4px;
+      padding: 10px 0 5px 10px;
+      box-shadow: 0 1px 3px rgba(0,0,0,.3);
+
+      word-break: break-word;
+
+      &--companion {
+        align-self: flex-start;
+        background-color: #fff;
+      }
+      &--me {
+        align-self: flex-end;
+        background-color: #DCF8C6;
+      }
+    }
+    &-text {
+      display: inline-block;
+      line-height: 1;
+      padding-right: 10px;
+    }
+    &-time {
+      font-size: 10px;
+      padding-right: 10px;
+    }
+  }
+  &__actions {
+    margin-top: 15px;
+    display: flex;
+    &-message {
+      flex-grow: 1;
+    }
+  }
+}
+
+</style>
